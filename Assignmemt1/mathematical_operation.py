d0cc9524e1735327785a48d936df1d157f4ec17b
vikastlearner <vikas.s.tiwari@zohomail.in>
2026-02-11 20:34:01 +0530

    Assignment 1

---
+++ Assignmemt1/mathematical_operation.py
@@ -0,0 +1,9 @@
+# Taking two numbers from user
+a=int(input("Enter first number: "))
+b=int(input("Enter second number: "))
+
+# Performing mathematical operation from the two numbers taken
+print("Addition: ",a+b)
+print("Subtraction: ",a-b)
+print("Multiplication: ",a*b)
+print("Division: ",a/b)
---
+++ Assignmemt1/welcome_message.py
@@ -0,0 +1,6 @@
+# Getting First Name and Last Name from user.
+first_name= input("Enter your first name: ")
+last_name = input("Enter your last name: ")
+
+# Printing the Welcome message.
+print("Hello, ",first_name +' '+ last_name + "! Welcome to the Python program")
