678224ca65ec9e6ffba7b7e02288a8e97cd914bc
vikastlearner <vikas.s.tiwari@zohomail.in>
2026-03-08 20:42:29 +0530

    Assignment 5

---
+++ Assignment5/dict-student_marks.py
@@ -0,0 +1,25 @@
+"""
+This file contains dictionary and fetching the value of dictionary.
+"""
+
+# Student details with their marks in Phy, Chem, Math, Bio, English and IT
+student_marks = {"Ravi": {"Physics": 80,"Chemistry": 83,"Maths": 87,"Biology": 70,"English": 75,"Information Tech": 83},
+                "Aniket": {"Physics": 86,"Chemistry":66,"Maths": 77,"Biology": 81,"English": 71,"Information Tech": 81},
+                "Sumeet": {"Physics": 92,"Chemistry": 91,"Maths": 95,"Biology": 85,"English": 92,"Information Tech": 99},
+                "Pradeep": {"Physics": 89,"Chemistry": 92, "Maths": 90,"Biology": 88,"English": 99,"Information Tech": 88},
+                "Shivam": {"Physics": 95,"Chemistry": 90,"Maths": 98,"Biology": 82,"English": 88,"Information Tech": 85}}
+
+name = input("Please your name: ")
+if name in student_marks:
+    marks = student_marks[name]
+    sum = sum(marks.values())
+    percentage = round(((sum/600)*100), 2)
+    for i in marks:
+        print(f"{i}: {marks[i]}/100")
+    print("=====================================================")
+    print(f"Total marks: {sum}/600 and Percentage: {percentage}%")
+    print("=====================================================")
+
+else:
+    print("Sorry, your name does not exist in list")
+
---
+++ Assignment5/list_slicing.py
@@ -0,0 +1,17 @@
+"""
+This file has list
+Operation such as slicing and reversing of the extracted list will be done
+"""
+
+list = [1,2,3,4,5,6,7,8,9,10]
+print(f"The original list is: {list}")
+
+# slicing
+sl = list[0:5]
+print(f"The sliced list is: {sl}")
+
+# reverse()
+sl.reverse()
+print(f"The reversed list is: {sl}")
+
+
